6936bc11e3b7d68d98156fc95a9e1c285c8cfc3f
OMRYZUTA <omrizu@mta.ac.il>
2021-06-25 22:52:59 +0300

    added requirements.txt, tests

--- s/tests.py
+++ s/tests.py
@@ -1,16 +1,39 @@
 from django.test import TestCase
 from .models import UsedUrlData
 from .views import urlShort, urlRedirect
+from django.http.response import Http404
 
 LONG_URL1 = "http://www.google.com/"
 SHORT_URL1 = "234djk"
 
+LONG_URL2 = "http://www.youtube.com/"
+SHORT_URL_LENGTH = 6
+RELATIVE_URL = 'http://localhost:8000/s/'
 
-class UsedUrlData(TestCase):
+class TestUsedUrlData(TestCase):
     def setUp(self):
-        UsedUrlData.objects.create(
-            url=LONG_URL1, short_url=SHORT_URL1)
+        pass
 
-    def test_url_redirect(self):
-        data = UsedUrlData.get(short_url=SHORT_URL1)
-        self.assertEqual(data.url, LONG_URL1)
+    def test_create_url_data(self):
+        new_url_object = UsedUrlData(url=LONG_URL2)
+        new_url_object.get_short_url()
+        new_url_object.save()
+        result = urlRedirect(None, new_url_object.short_url)
+        self.assertEqual(result.url, LONG_URL2)
+
+    def test_non_exist_short_url(self):
+        new_url_object = UsedUrlData(url=LONG_URL2)
+        non_exist_short = new_url_object.generate_short_url()
+        self.assertTrue(len(UsedUrlData.objects.filter(
+            short_url=non_exist_short)) == 0)
+
+    def test_404_on_non_exist_short_url(self):
+        new_url_object = UsedUrlData(url=LONG_URL2)
+        non_exist_short = new_url_object.generate_short_url()
+        result = urlRedirect(None, non_exist_short)
+        self.assertTrue(isinstance(result, Http404))
+
+    def test_short_url_is_short(self):
+        new_url_object = UsedUrlData(url=LONG_URL2)
+        new_url = new_url_object.get_short_url()
+        self.assertEqual(len(new_url),SHORT_URL_LENGTH+ len(RELATIVE_URL))
